2ad49c48eed374248d454636da7fa4e155bf5e81
Kevin Lin <kwkevinlin@users.noreply.github.com>
2016-03-10 16:59:14 -0500

    Changed approach, more efficient

--- Anagram_Buckets.cpp
+++ Anagram_Buckets.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <list>
-#include <unordered_map>
+#include <algorithm>
+#include <string>
 
 using namespace std;
 
@@ -10,26 +11,31 @@ int main() {
 	//Given a list of words, output buckets for set of words that are anagrams of each other.
 	//For example, given the list of words {foo, ofo, bar} return two lists containing {foo, ofo} and {bar}
 
-	list<string> words;
-	words.push_back("foo");
-	words.push_back("ofo");
-	words.push_back("bar");
+	list<string> input;
+	input.push_back("foo");
+	input.push_back("ofo");
+	input.push_back("bar");
 
 	/*
-	 * Simple Approach First: Create hash for each word.
-	 * For each word, traverse through other hashes to check if is anagram
-	*/
+	 * Second Approach: Sort each word, then compare.
+	 * Sorted words == same, if they are anagrams (string has == function)
+	 */
+
+	string words[input.size()];
+
+	//Sorting each word
 	int i = 0;
-	unordered_map<char, int> hashArray[words.size()];
-	while (words.empty() == 0) {
-		string word = words.front();
-		for (int j = 0; j < word.length(); j++) {
-			hashArray[i][word[j]]++; //Storing chars into appropriate hash
-		}
-		words.pop_front();
+	while (input.empty() != 1) {
+		words[i] = input.front();
+		sort(words[i].begin(), words[i].end());
+		input.pop_front();
 		i++;
 	}
 
+	//Test sort
+	for (i = 0; i < sizeof(words)/sizeof(words[0]); i++) {
+		cout << words[i];
+	}
 
 
 }
