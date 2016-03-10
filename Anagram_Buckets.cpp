87b42a61c15c2b57116da1f8e272c6ff573baa06
Kevin Lin <kwkevinlin@users.noreply.github.com>
2016-03-10 17:14:35 -0500

    Changed input to array, started categorizing buckets

--- Anagram_Buckets.cpp
+++ Anagram_Buckets.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <list>
+#include <unordered_map>
 #include <algorithm>
 #include <string>
 
@@ -11,31 +12,41 @@ int main() {
 	//Given a list of words, output buckets for set of words that are anagrams of each other.
 	//For example, given the list of words {foo, ofo, bar} return two lists containing {foo, ofo} and {bar}
 
-	list<string> input;
-	input.push_back("foo");
-	input.push_back("ofo");
-	input.push_back("bar");
+	string input[3] = {"foo", "ofo", "bar"};
 
 	/*
 	 * Second Approach: Sort each word, then compare.
 	 * Sorted words == same, if they are anagrams (string has == function)
 	 */
 
-	string words[input.size()];
+	int length = sizeof(input)/sizeof(input[0]);
+	string words[length];
 
 	//Sorting each word
-	int i = 0;
-	while (input.empty() != 1) {
-		words[i] = input.front();
+	for (int i = 0; i < length; i++) {
+		words[i] = input[i];
 		sort(words[i].begin(), words[i].end());
-		input.pop_front();
-		i++;
 	}
 
-	//Test sort
-	for (i = 0; i < sizeof(words)/sizeof(words[0]); i++) {
-		cout << words[i];
+	//Test sorts
+	for (int i = 0; i < length; i++) {
+		cout << words[i] << endl;
 	}
+
+	//Categorizing to buckets
+	unordered_map<string, list<string>> buckets;
+	for (int i = 0; i < length; i++) {
+		buckets[words[i]].push_back(input[i]);
+	}
+
+	//Printout Buckets to get Anagram groupings
+
+
+//	for (auto itr = buckets.begin(); itr != buckets.end(); ++itr) {
+//		for (int i = 0; i < itr->size(); i++) {
+//			cout << itr[i] << endl;
+//		}
+//	}
 
 
 }
