0ffaf47656ed47bb5a815ce7aaa11a19f5bd0ac0
Kevin Lin <kwkevinlin@users.noreply.github.com>
2016-03-12 01:31:21 -0500

    Completed intersection

--- Union_and_Intersection_of_Arrays.cpp
+++ Union_and_Intersection_of_Arrays.cpp
@@ -12,16 +12,24 @@ int main() {
 	 * Union and Intersection of two arrays
 	 *
 	 * Definitions:
-	 *     Union = All of in arr1, arr2, and in both.
+	 *     Union = All of arr1, arr2, and included in both.
 	 *     Intersection = Only those in both arr1 and arr2.
 	 *
 	 * Notes:
-	 *     Union and intersection contains no duplicate.
-	 *     Unordered_set bucket outputs are unordered.
+	 *     Union and intersection contains no duplicate
+	 *     Unordered_set bucket outputs are unordered
+	 *
+	 * Input:
+	 *     array1 = {7, 5, 3, 4, 9, 1}
+	 *     array2 = {2, 3, 0, 9, 6, 8}
+	 *
+	 * Output:
+	 *     Union: 0 1 2 3 4 5 6 7 8 9
+     *     Intersection: 3 9
 	 */
 
 	int arr1[] = {7, 5, 3, 4, 9, 1};
-	int arr2[] = {2, 3, 9, 0, 6, 8};
+	int arr2[] = {2, 3, 0, 9, 6, 8};
 
 	int length = sizeof(arr1)/sizeof(arr1[0]);
 
@@ -43,7 +51,7 @@ void getUnion(int arr1[], int arr2[], int length) {
 	}
 
 	//Union printout
-	cout << "Unions: ";
+	cout << "Union: ";
 	for (auto kv : setUnion) {
 		cout << kv << " ";
 	}
@@ -52,5 +60,18 @@ void getUnion(int arr1[], int arr2[], int length) {
 
 void getIntersection(int arr1[], int arr2[], int length) {
 
+	unordered_set<int> setInter;
+
+	for (int i = 0; i < length; i++) {
+		setInter.insert(arr1[i]);
+	}
+
+	cout << "Intersection: ";
+	for (int i = 0; i < length; i++) {
+		if (setInter.count(arr2[i]) > 0) { //If key exists
+			cout << arr2[i] << " ";
+		}
+	}
+	cout << endl;
 
 }
