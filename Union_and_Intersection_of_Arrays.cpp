b5f7db068153859be7829394725b96aadea4242e
Kevin Lin <kwkevinlin@users.noreply.github.com>
2016-03-12 01:24:09 -0500

    Completed Unions

--- Union_and_Intersection_of_Arrays.cpp
+++ Union_and_Intersection_of_Arrays.cpp
@@ -11,10 +11,13 @@ int main() {
 	/*
 	 * Union and Intersection of two arrays
 	 *
-	 * Union = All of in arr1, arr2, and in both.
-	 * Intersection = Only those in both arr1 and arr2.
+	 * Definitions:
+	 *     Union = All of in arr1, arr2, and in both.
+	 *     Intersection = Only those in both arr1 and arr2.
 	 *
-	 * Unions and intersections contain no duplicates.
+	 * Notes:
+	 *     Union and intersection contains no duplicate.
+	 *     Unordered_set bucket outputs are unordered.
 	 */
 
 	int arr1[] = {7, 5, 3, 4, 9, 1};
@@ -31,9 +34,20 @@ void getUnion(int arr1[], int arr2[], int length) {
 
 	unordered_set<int> setUnion;
 
-	for (int i = 0; i < length; i++) {
+	//Not sure yet the error of range insert
+	//setUnion.insert(arr1->begin(), arr1.end());
 
+	for (int i = 0; i < length; i++) {
+		setUnion.insert(arr1[i]);
+		setUnion.insert(arr2[i]);
 	}
+
+	//Union printout
+	cout << "Unions: ";
+	for (auto kv : setUnion) {
+		cout << kv << " ";
+	}
+	cout << endl;
 }
 
 void getIntersection(int arr1[], int arr2[], int length) {
