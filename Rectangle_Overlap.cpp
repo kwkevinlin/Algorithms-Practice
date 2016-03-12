dc767738cf5ad0eb72c37b9a6e2e66c462e8a4f5
Kevin Lin <kwkevinlin@users.noreply.github.com>
2016-03-12 00:59:15 -0500

    Started rectangle overlap

---
+++ Rectangle_Overlap.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+
+	/*
+	 * Given a spring: "(1,2),(1,1)..." that represents 8 coordinates, the first four is for
+	 * the first rectangle, the next four is for the second rectangle.
+	 *
+	 * Determine 1). if the rectangles overlap, and
+	 * 			 2). if 1). is true, what percent of the first rectangle is overlapped
+	 *
+	 * For example:
+	 *     Input string: "(1,1),(4,4),(4,1),(1,4),(4,2),(3,2),(4,1),(3,1)" (coordinates are
+	 *     				 in no order except for first four = 1st rectangle, ... etc)
+	 *
+	 *         Visualized: (Rect 1 = Big one, Rect 2 = Small one)
+	 *					    
+	 *         			 4 |-----|
+	 *         			 3 |     |
+	 *         			 2 |    _|
+	 *         			 1 |___|_|
+	 *         			   1 2 3 4
+	 *     Output:
+	 *     		1). Yes
+	 *     		2). 1/9 = 11% overlapped
+	 *
+	 */
+
+
+
+
+}
