8508ffad2c902b68bd3a724d4f6f38ed59f1a22e
Kevin Lin <kwkevinlin@users.noreply.github.com>
2016-03-20 01:10:15 -0400

    Completed even palindrome, and updated spelling :( whoops

--- Longest_Palindrome_Subsequence.cpp
+++ Longest_Palindrome_Subsequence.cpp
@@ -2,59 +2,67 @@
 
 using namespace std;
 
-string longestPalimdrome(string);
+string longestPalindrome(string);
 
 int main() {
 
 	/*
-	 * Find the Longest Palimdrome in a string
-	 *     This solution find the longest palimdrone in O(n^2) time
+	 * Find the Longest Palindrome in a string
+	 *     This solution find the longest palindrome in O(n^2) time
 	 *     Manacher’s Algorithm can be used the find the same thing in O(n), linear time
 	 *
 	 */
 
-	//string str = "forgeeksskeegfor"; //Palimdrome: "geeksskeeg"
-	string str = "foriaifor"; //iai
+	string str = "forgeeksskeegfor"; //Palimdrome: "geeksskeeg"
+	//string str = "foriifor"; //ii - Even
 
-	cout << longestPalimdrome(str) << endl;
+	cout << "Longest palindromic subsequence: \"" << longestPalindrome(str) << "\"" << endl;
 
 }
 
-string longestPalimdrome(string str) {
+string longestPalindrome(string str) {
 
-	//Check for both EVEN and ODD palimdromes for each "center", ie each [i] in str
-	int start, end, max = -1, count = 0;
+	//Check for both EVEN and ODD palindromes for each "center", ie each [i] in str
+	int start, end, count, max = -1;
 	int palinStart, length;
 
-	//From 1 to length - 1, use each as center to check for palimdrome
+	//From 1 to length - 1, use each as center to check for palindrome
 	for (int i = 1; i < str.length() - 1; i++) {
+
+		cout << "Loop " << i << ": " << str[i] << endl;
 
 		/* Even sequence */
 		start = i - 1;
 		end = i;
-		while (start < str.length() && end >= 0) {
+		count = 0;
+		while (start >= 0 && end < str.length()) {
 			if (str[start] == str[end]) {
+				cout << "    Match-> " << str[start] << " : " << str[end] << endl;
 				count++;
 
 				if (count > max) {
 					max = count; 			//Current max palindrome length
 					palinStart = i - count;	//Starting index of this palindrome
-					length = 2 * count + 1;	//Number of chars
+					length = 2 * count;	//For str.substring(palinStart, length)
+					cout << "\t    Current palin length: " << length << endl;
 				}
+				start--;
+				end++;
+			} else {
+				break;
 			}
-			start--;
-			end++;
+
 		}
 
-		max = -1;
 		count = 0;
 
 		/* Odd Sequence */
 		start = i - 1;
 		end = i + 1;
 
-
 	}
+
+	cout << "\nPalinStart: " << palinStart << ", length: " << length << endl << endl;
 
 	return str.substr(palinStart, length);
 
