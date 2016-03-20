a3205f601042218e5d5d0b51a155ca8fc8a5fb09
Kevin Lin <kwkevinlin@users.noreply.github.com>
2016-03-19 21:16:50 -0400

    Started function, gets corrects head

--- Reverse_Linked_List.cpp
+++ Reverse_Linked_List.cpp
@@ -8,13 +8,17 @@ struct Node
 	int data;
 	Node* next;
 
+	Node() {
+		data = -1;
+		next = NULL;
+	}
 	Node(int n) {
 		data = n;
 		next = NULL;
 	}
 };
 
-void reverseList(Node*);
+void reverseList(Node*, Node*, Node**);
 
 int main() {
 
@@ -22,12 +26,28 @@ int main() {
 
 	/* Populate list without linked list class */
 	head->next = new Node(2);
-	head->next->next = new Node(2);
+	head->next->next = new Node(3);
 	head->next->next->next = new Node(4);
 	head->next->next->next->next = new Node(5);
 	head->next->next->next->next->next = new Node(6);
 	head->next->next->next->next->next->next = new Node(7);
 	head->next->next->next->next->next->next->next = new Node(8);
 
-	reverseList(head);
+	Node* reversedList;
+	reverseList(head, NULL, &reversedList); //Passing reversedList by reference to update head
+
+	cout << "Rev: " << reversedList->data << endl;
+}
+
+void reverseList(Node* current, Node* prev, Node** head) {
+
+	if (current->next == NULL) { //If last element
+		*head = current; //Update head
+	} else {
+		reverseList(current->next, current, head);
+	}
+
+	//Tail recursion returns
+	current->next = prev;
+
 }
