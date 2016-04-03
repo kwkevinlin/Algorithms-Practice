d3103bf4030ad0520bd71758497c8f41f243ce54
Kevin Lin <kwkevinlin@users.noreply.github.com>
2016-04-02 23:45:48 -0400

    Problem setup

---
+++ Brackets-Parenthesis_Checker.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	/*
+	 * Match the braces and verify whether all the opening braces have the closing braces in the right order.
+	 *
+	 * For example:
+	 *     Input: [{}]()[{{()}}()]
+	 *     Output: Yes
+	 *
+	 *     Input: [{}]({{()}}()]
+	 *     Output: No
+	 *
+	 */
+
+
+
+
+}
