4c675d15000617816c9fb611b09088fe8b1aeb77
Kevin Lin <kwkevinlin@users.noreply.github.com>
2016-04-02 23:56:44 -0400

    Added hash and main function

--- Brackets-Parenthesis_Checker.cpp
+++ Brackets-Parenthesis_Checker.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
+#include <stack>
+#include <unordered_map>
 
 using namespace std;
+
+bool symbolChecker(string);
 
 int main() {
 
@@ -16,7 +20,43 @@ int main() {
 	 *
 	 */
 
+	string input = "[()]";
+	//string input = "[{}]()[{{()}}()]";
 
+	if (symbolChecker(input)) {
+		cout << "Symbols match!\n";
+	} else {
+		cout << "Symbols do NOT match!\n";
+	}
 
+}
+
+bool symbolChecker(string str) {
+
+	stack<char> symStack;
+	unordered_map<char, char> symbols;
+
+	//Storing complements to hash table
+	symbols['{'] = '}';
+	symbols['['] = ']';
+	symbols['('] = ')';
+
+	//If opening, push onto stack. If closing, check top of stack.
+	for (int i = 0; i < str.length(); i++) {
+		//If opening
+		if (str[i] == '[' || str[i] == '{' || str[i] == '(') {
+			symStack.push(str[i]);
+		}
+		//If closing
+		else {
+			if (symStack.top() == str[i]) {
+				symStack.pop();
+			} else {
+				return false;
+			}
+		}
+	}
+
+	return true;
 
 }
