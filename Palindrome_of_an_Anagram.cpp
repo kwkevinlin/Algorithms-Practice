cc493f7fb888b182670367799c5ad86f9411407f
Kevin Lin <kwkevinlin@users.noreply.github.com>
2016-04-03 01:24:26 -0400

    In progress of attempt 1

--- Palindrome_of_an_Anagram.cpp
+++ Palindrome_of_an_Anagram.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
+#include <unordered_map>
 
 using namespace std;
+
+bool palindromeOfAnagram(string);
 
 int main() {
 
@@ -25,4 +28,39 @@ int main() {
 	 *     HackerRank - Game of Thrones - I
 	 *     https://www.hackerrank.com/challenges/game-of-thrones
 	 */
+
+	string str = "aaabbbb";
+
+	if (palindromeOfAnagram(str)) {
+		cout << "YES\n" << endl;
+	} else {
+		cout << "NO\n" << endl;
+	}
+
+}
+
+bool palindromeOfAnagram(string str) {
+
+	unordered_map<char, int> charMap;
+
+	//Add every char to map
+	for (int i = 0; i < str.length(); i++) {
+		charMap[str[i]]++;
+	}
+
+	int oddCount = 0;
+
+	for (int i = 0; i < str.length(); i++) {
+		cout << str[i] << ", " << charMap[str[i]] << endl;
+		if (charMap[str[i]] % 2 == 1) {
+			oddCount++;
+		}
+	}
+
+	if (oddCount > 1) {
+		return false;
+	} else {
+		return true;
+	}
+
 }
