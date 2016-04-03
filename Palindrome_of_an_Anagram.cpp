90d7ec653aea9722c46d6ed462b8b8ade3cb7a1b
Kevin Lin <kwkevinlin@users.noreply.github.com>
2016-04-03 01:17:49 -0400

    Problem setup

---
+++ Palindrome_of_an_Anagram.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	/*
+	 * Determine whether an anagram can be a palindrome as well.
+	 *
+	 * Example:
+	 *     Input: aaabbbb
+	 *     Output: Yes
+	 *     Explanation: A palindrome permutation of the string
+	 *                  can be: bbaaabb.
+	 *
+	 *     Input: cdcdcdcdeeeef
+	 *     Output: Yes
+	 *     Explanation: Palindrome sequence = ddcceefeeccdd
+	 *
+	 *     Input: cdefghmnopqrstuvw
+	 *     Output: No
+	 *     Explanation: No possible palindrome sequence
+	 *
+	 * Question adapted from:
+	 *     HackerRank - Game of Thrones - I
+	 *     https://www.hackerrank.com/challenges/game-of-thrones
+	 */
+}
