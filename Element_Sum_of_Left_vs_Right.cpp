3344997ea10536c74a84478d03c4c43357258989
Kevin Lin <kwkevinlin@users.noreply.github.com>
2016-04-03 02:03:35 -0400

    Problem setup

---
+++ Element_Sum_of_Left_vs_Right.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	/*
+	 * Determine if there is an element in the array such that the
+	 * sum of the elements on the left == sum of the elements on 
+	 * the right.
+	 *
+	 * Example:
+	 *     Input: 2        //Number of test cases
+	 *            3        //Length of following case
+	 *            1 2 3    //Test case
+	 *            4
+	 *            1 2 3 3
+	 *     Output: NO
+	 *             YES
+	 *     Explanation:
+	 *             Case 1: No such element
+	 *             Case 2: [2], because (left) 1 + 2 == (right) 3
+	 *
+	 *
+	 * Question adapted from:
+	 *     HackerRank - Sherlock and Array
+	 *     https://www.hackerrank.com/challenges/sherlock-and-array
+	 *  
+	 */
+
+
+
+
+	 2
+3
+1 2 3
+4
+1 2 3 3
+
+}
