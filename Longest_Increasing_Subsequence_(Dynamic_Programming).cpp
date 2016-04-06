05c12bb228d8ebe6ab0a367a108470610cf32ce5
Kevin Lin <kwkevinlin@users.noreply.github.com>
2016-04-06 00:02:46 -0400

    Problem setup

---
+++ Longest_Increasing_Subsequence_(Dynamic_Programming).cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	/*
+	 * Find the Longest Increasing Subsequence using
+	 * dynamic programming.
+	 *
+	 * For example:
+	 *     5 2 8 6 3 6 9 7
+	 *       >     > > >
+	 *
+	 * Longest sequence: 4
+	 *     From 2 -> 3 -> 6 -> 9
+	 *
+	 */
+}
