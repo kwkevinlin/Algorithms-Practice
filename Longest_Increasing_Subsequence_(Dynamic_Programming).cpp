fc0c8d715417d8a33d5e32c5fcb49b9439c04fff
Kevin Lin <kwkevinlin@users.noreply.github.com>
2016-04-06 00:18:05 -0400

    Started method 1: adjacency list

--- Longest_Increasing_Subsequence_(Dynamic_Programming).cpp
+++ Longest_Increasing_Subsequence_(Dynamic_Programming).cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <algorithm>
+#include <iterator>
 
 using namespace std;
 
@@ -15,5 +17,25 @@ int main() {
 	 * Longest sequence: 4
 	 *     From 2 -> 3 -> 6 -> 9
 	 *
+	 * Method 1:
+	 *     Create adjacency list of smaller elements,
+	 *     then use standard DP to solve
 	 */
+
+	int arr[] = {5, 2, 8, 6, 3, 6, 9, 7};
+	int size = sizeof(arr)/sizeof(arr[0]);
+	int max[size];
+
+	max[0] = 1;
+
+	//Create adjacency list, where edge = all elements smaller
+	int arrSorted[size];
+	copy(arr, arr + size, arrSorted);
+
+	//Sort array to get all smaller elements easier
+	sort(arrSorted, arrSorted + size);
+
+	for (int i = 1; i < sizeof(arr)/sizeof(arr[0]); i++) {
+
+	}
 }
