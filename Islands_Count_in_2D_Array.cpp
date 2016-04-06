330a303b5a2d7d6807809b334a34a943e3db469b
Kevin Lin <kwkevinlin@users.noreply.github.com>
2016-04-05 23:49:59 -0400

    Completed discover(), added solution explanation

--- Islands_Count_in_2D_Array.cpp
+++ Islands_Count_in_2D_Array.cpp
@@ -21,6 +21,16 @@ int main() {
 	 *      {0, 0, 0, 1}}
 	 *
 	 *     Number of islands will be 3
+	 *
+	 * Solution:
+	 *     2D loop through the array. For each 1 found,
+	 *     call discover() on that index. In discover,
+	 *     modify value of 1 to -1 so that [i][j] wont,
+	 *     be visited again. Then, still in discover,
+	 *     check all 4 directions (up, down, left, right)
+	 *     to check if there are any connecting lands.
+	 *     If there are, call discover on the index (i,j)
+	 *     of that land.
 	 */
 
 	int land[numHeight][numWidth] = {
@@ -32,12 +42,12 @@ int main() {
 
 
 	int islands = countIslands(land);
-	cout << "Number of islands in 2D array:\n" << islands << endl;
+	cout << "Number of islands:\n" << islands << endl;
 
 }
 
 int countIslands(int land[][numWidth]) {
-	int islands;
+	int islands = 0;
 
 	for (int i = 0; i < numHeight; i++) {
 		for (int j = 0; j < numWidth; j++) {
@@ -50,8 +60,31 @@ int countIslands(int land[][numWidth]) {
 		}
 	}
 
+	return islands;
+
 }
 
 void discover(int land[][numWidth], int i, int j) {
 	//Check all four directions. Visited, mark as -1
+
+	//Left
+	if (j - 1 >= 0 && land[i][j-1] == 1) {
+		land[i][j-1] = -1;
+		discover(land, i, j-1);
+	}
+	//Right
+	if (j + 1 < numWidth && land[i][j+1] == 1) {
+		land[i][j+1] = -1;
+		discover(land, i, j+1);
+	}
+	//Up
+	if (i - 1 >= 0 && land[i-1][j] == 1) {
+		land[i-1][j] = -1;
+		discover(land, i-1, j);
+	}
+	//Down
+	if (i + 1 < numHeight && land[i+1][j] == 1) {
+		land[i+1][j] = -1;
+		discover(land, i+1, j);
+	}
 }
