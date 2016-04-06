9773ebe3d462136b503354601c1ac1651bc3f260
Kevin Lin <kwkevinlin@users.noreply.github.com>
2016-04-06 00:37:32 -0400

    Find length completed, using different method

--- Longest_Increasing_Subsequence_(Dynamic_Programming).cpp
+++ Longest_Increasing_Subsequence_(Dynamic_Programming).cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 #include <algorithm>
 #include <iterator>
 
@@ -9,33 +10,39 @@ int main() {
 	/*
 	 * Find the Longest Increasing Subsequence using
 	 * dynamic programming.
+	 *     Then print out path.
 	 *
 	 * For example:
 	 *     5 2 8 6 3 6 9 7
 	 *       >     > > >
+	 *       >     > >   >
 	 *
 	 * Longest sequence: 4
-	 *     From 2 -> 3 -> 6 -> 9
+	 *     From 2 -> 3 -> 6 -> 9   (or)
+	 *     From 2 -> 3 -> 6 -> 7
 	 *
-	 * Method 1:
-	 *     Create adjacency list of smaller elements,
-	 *     then use standard DP to solve
 	 */
 
 	int arr[] = {5, 2, 8, 6, 3, 6, 9, 7};
-	int size = sizeof(arr)/sizeof(arr[0]);
-	int max[size];
 
-	max[0] = 1;
+	int maxArr[sizeof(arr)/sizeof(arr[0])];
+	int max = -1;
 
-	//Create adjacency list, where edge = all elements smaller
-	int arrSorted[size];
-	copy(arr, arr + size, arrSorted);
+	//DP from start to end, O(n^2) solution
+	for (int i = 0; i < sizeof(arr)/sizeof(arr[0]); i++) {
+		//DP base case
+		maxArr[i] = 1;
+		for (int j = 0; j < i; j++) {
+			if (arr[j] < arr[i]) {
+				maxArr[i] = maxArr[j] + 1;
+				cout << i << " " << j << ", " << maxArr[i] << endl;
 
-	//Sort array to get all smaller elements easier
-	sort(arrSorted, arrSorted + size);
-
-	for (int i = 1; i < sizeof(arr)/sizeof(arr[0]); i++) {
-
+				if (maxArr[i] > max) {
+					max = maxArr[i];
+				}
+			}
+		}
 	}
+
+	cout << max << endl;
 }
