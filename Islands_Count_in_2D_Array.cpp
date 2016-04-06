8344d99a7a9096d961a3bc7dfcf3d0b239ed009c
Kevin Lin <kwkevinlin@users.noreply.github.com>
2016-04-05 23:21:24 -0400

    Added countIsland() and discover() function

--- Islands_Count_in_2D_Array.cpp
+++ Islands_Count_in_2D_Array.cpp
@@ -2,6 +2,11 @@
 
 using namespace std;
 
+const int numWidth = 4;
+const int numHeight = 4;
+void discover(int[][numWidth], int, int);
+int countIslands(int[][numWidth]);
+
 int main() {
 
 	/*
@@ -10,13 +15,43 @@ int main() {
 	 *     Island = 1s
 	 *
 	 *     For example:
-	 *     {[1, 1, 0, 1],
-	 *      [0, 1, 0, 0],
-	 *      [0, 0, 0, 0],
-	 *      [0, 0, 0, 1]}
+	 *     {{1, 1, 0, 1},
+	 *      {0, 1, 0, 0},
+	 *      {0, 0, 0, 0},
+	 *      {0, 0, 0, 1}}
 	 *
 	 *     Number of islands will be 3
 	 */
 
+	int land[numHeight][numWidth] = {
+			{1, 1, 0, 1},
+			{0, 1, 0, 0},
+			{0, 0, 0, 0},
+			{0, 0, 0, 1}
+	};
 
+
+	int islands = countIslands(land);
+	cout << "Number of islands in 2D array:\n" << islands << endl;
+
+}
+
+int countIslands(int land[][numWidth]) {
+	int islands;
+
+	for (int i = 0; i < numHeight; i++) {
+		for (int j = 0; j < numWidth; j++) {
+
+			if (land[i][j] == 1) {
+				discover(land, i, j);
+				islands++;
+			}
+
+		}
+	}
+
+}
+
+void discover(int land[][numWidth], int i, int j) {
+	//Check all four directions. Visited, mark as -1
 }
