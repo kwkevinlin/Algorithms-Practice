a79f8d7e526e4f6a3e22252273ccb9815e0a7a95
Kevin Lin <kwkevinlin@users.noreply.github.com>
2016-04-05 23:11:53 -0400

    Problem setup

---
+++ Islands_Count_in_2D_Array.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	/*
+	 * Given a 2D array of 1s and 0s, identify
+	 * the number of distinct islands in the array.
+	 *     Island = 1s
+	 *
+	 *     For example:
+	 *     {[1, 1, 0, 1],
+	 *      [0, 1, 0, 0],
+	 *      [0, 0, 0, 0],
+	 *      [0, 0, 0, 1]}
+	 *
+	 *     Number of islands will be 3
+	 */
+
+
+}
