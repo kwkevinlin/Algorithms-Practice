35888ab957667e2f706302c0bd96babb66e8a2bf
Kevin Lin <kwkevinlin@users.noreply.github.com>
2016-04-06 23:20:42 -0400

    Problem setup

---
+++ Sort_Stack_With_Stack.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+int longestIncSubseq(int[], int);
+
+int main() {
+
+	/*
+	 * Write a program to sort a stack in ascending order (biggest
+	 * items on top). You may use at most one additional stack to
+	 * hold items, but you may not copy the elements into any other
+	 * data structure.
+	 *
+	 * Adapted from Cracking the Coding Interview
+	 * Chapter 3, Question 6
+	 *
+	 */
+
+	stack<int> stack;
+	stack.push(1);
+	stack.push(3);
+	stack.push(5);
+	stack.push(7);
+
+
+}
