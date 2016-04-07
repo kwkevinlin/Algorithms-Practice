315431412321e593c6d03ac5f2352c820c7b0fed
Kevin Lin <kwkevinlin@users.noreply.github.com>
2016-04-07 00:12:02 -0400

    Completed algorithm and added solution/complexity description

--- Sort_Stack_With_Stack.cpp
+++ Sort_Stack_With_Stack.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-int longestIncSubseq(int[], int);
+void sortStack(stack<int>, stack<int>&);
 
 int main() {
 
@@ -12,6 +12,16 @@ int main() {
 	 * items on top). You may use at most one additional stack to
 	 * hold items, but you may not copy the elements into any other
 	 * data structure.
+	 *
+	 * Solution:
+	 *     If stack1.top > stack2.top, push stack1.top into stack2.top.
+	 *     Else, store stack1.top into a tmp variable, then push every
+	 *     element on stack2 > tmp to stack1. Then, push tmp onto stack2.
+	 *     Repeat process until stack1 is empty.
+	 *
+	 * Complexity:
+	 *     Time: O(n^2)
+	 *     Space: O(n)
 	 *
 	 * Adapted from Cracking the Coding Interview
 	 * Chapter 3, Question 6
@@ -24,24 +34,46 @@ int main() {
 	stack1.push(7);
 	stack1.push(1);
 
-	stack<int> stack2;
+	stack<int> result;
+	sortStack(stack1, result);
 
-	//So stack2 already has an element
-	int tmp = stack1.top();
+	//Print results
+	while (result.empty() != true) {
+		cout << result.top() << " ";
+		result.pop();
+	}
+
+}
+
+void sortStack(stack<int> stack1, stack<int> &result) {
+
+	int tmp;
+
+	//Populate result stack with top element of stack1
+	result.push(stack1.top());
 	stack1.pop();
 
 	//While stack1 still has elements
 	while (stack1.empty() != true) {
-		if (stack1.top() > stack2.top()) {
-			//Move stack1 element to stack2
+		if (stack1.top() >= result.top()) {
+			//Move stack1 element to result
+			result.push(stack1.top());
+			stack1.pop();
+		} else {
+			/*  Pop stack1.top in tmp, then push every
+					element greater than tmp in result onto stack1. Then,
+					push tmp into result.  */
 			tmp = stack1.top();
 			stack1.pop();
-			stack2.push(tmp);
-		} else {
-			/*  Pop stack1.top in another var temp, then push every
-				element greater than temp in stack2 onto stack1. Then,
-				push temp into stack2.  */
-
+			while (result.empty() != true) {
+				if (result.top() > tmp) {
+					stack1.push(result.top());
+					result.pop();
+				} else {
+					break;
+				}
+			}
+			result.push(tmp);
 		}
 	}
 
