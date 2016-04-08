09644725b37712df1f73e86635ba223c5b721836
Kevin Lin <kwkevinlin@users.noreply.github.com>
2016-04-07 22:20:32 -0400

    Completed balanced BST build

--- Create_Balanced_BST_from_Array.cpp
+++ Create_Balanced_BST_from_Array.cpp
@@ -2,6 +2,10 @@
 
 using namespace std;
 
+struct Node;
+Node* buildTree(int[], int, int);
+void printTree(Node*);
+
 struct Node {
 	int data;
 	Node* left;
@@ -33,4 +37,36 @@ int main() {
 
 	int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
 
+	Node* head = buildTree(arr, 0, sizeof(arr)/sizeof(arr[0]) - 1);
+
+	//Check build with prefix printout
+	printTree(head);
+
+}
+
+Node* buildTree(int arr[], int start, int end) {
+
+	if (start > end) {
+		return NULL;
+	}
+	int mid = (start + end) / 2;
+
+	Node* newNode = new Node(arr[mid]);
+
+	newNode->left = buildTree(arr, start, mid-1);
+	newNode->right = buildTree(arr, mid+1, end);
+
+	return newNode;
+}
+
+void printTree(Node* itr) {
+
+	cout << itr->data << " ";
+
+	if (itr->left != NULL) {
+		printTree(itr->left);
+	}
+	if (itr->right != NULL) {
+		printTree(itr->right);
+	}
 }
