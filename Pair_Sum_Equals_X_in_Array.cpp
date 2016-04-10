0ff2958e58076ed92ec0f355ba8b89d568d745a7
Kevin Lin <kwkevinlin@users.noreply.github.com>
2016-04-09 22:43:38 -0400

    Problem setup

---
+++ Pair_Sum_Equals_X_in_Array.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <unordered_map>
+
+using namespace std;
+
+
+int main() {
+
+	/*
+	 * Given an int array, find if there is a pair of
+	 * for which the sum of the pair equals an integer
+	 * in the array itself
+	 */
+
+
+}
