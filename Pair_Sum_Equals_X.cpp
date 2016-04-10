9191a57b2a4e0f9809172508a0158edcb9bf4bf2
Kevin Lin <kwkevinlin@users.noreply.github.com>
2016-04-09 20:50:55 -0400

    Fixed segfault - accessing out of bounds vector

--- Pair_Sum_Equals_X.cpp
+++ Pair_Sum_Equals_X.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 #include <unordered_map>
 
 using namespace std;
@@ -24,21 +25,32 @@ int main() {
 
 		//Read flavors cost into cost[]
 		int cost[flavors];
-		unordered_map<int, int> hash;
+		unordered_map<int, vector<int>> hash;
 		for (int j = 0; j < flavors; j++) {
 			cin >> n;
 			cost[j] = n;
-			hash[n] = j;
+			hash[n].push_back(j + 1); //Since first element should output 1 not 0
 		}
 
+		//For every index, take sum - cost[index] and see if result is in the cost array
 		int tmp;
 		for (int j = 0; j < flavors; j++) {
 			tmp = sum - cost[j];
-			if (hash.count(tmp) > 0) {
-				if (cost[j] > tmp) {
-					cout << hash[tmp] + 1 << " " << hash[j] + 1 << endl;
-				} else {
-					cout << hash[j] + 1 << " " << hash[tmp] + 1 << endl;
+			if (hash.count(tmp) > 0) { //If such element exists
+
+				//If there's a repeat of that price
+				if (hash[tmp].size() > 1) { //Print [0] then [1]
+					fflush(stdout);
+					cout << hash[cost[j]][0] << " " << hash[tmp][1]; //where hash[cost[j]] == hash[tmp]
+				}
+				else { //Always print [0]
+
+					fflush(stdout);
+					if (hash[cost[j]][0] > hash[tmp][0]) {
+						cout << hash[tmp][0] << " " << hash[cost[j]][0] << endl;
+					} else {
+						cout << hash[cost[j]][0] << " " << hash[tmp][0] << endl;
+					}
 				}
 				break;
 			}
