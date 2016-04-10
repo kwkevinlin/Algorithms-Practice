d3f1f89903bde3794b6013ea63d03236cd048eca
Kevin Lin <kwkevinlin@users.noreply.github.com>
2016-04-09 22:54:13 -0400

    Completed algorithm

--- Pair_Sum_Equals_X_in_Array.cpp
+++ Pair_Sum_Equals_X_in_Array.cpp
@@ -10,7 +10,27 @@ int main() {
 	 * Given an int array, find if there is a pair of
 	 * for which the sum of the pair equals an integer
 	 * in the array itself
+	 *
+	 * Complexity:
+	 *     Time: O(n^2) because of pairing. Hash check
+	 *           is O(1) constant time.
 	 */
 
+	int arr[] = {4, 6, 2, 8, 7, 1};
+
+	//Store each int in set for easier lookup
+	unordered_map<int, int> hash;
+	for (int i = 0; i < sizeof(arr)/sizeof(arr[0]); i++) {
+		hash[arr[i]] = 1;
+	}
+
+	//Make each pairs and check if sum is in the set
+	for (int i = 0; i < sizeof(arr)/sizeof(arr[0]); i++) {
+		for (int j = i + 1; j < sizeof(arr)/sizeof(arr[0]); j++) {
+			if (hash.count(arr[i] + arr[j]) >= 1) { //If found in hash
+				cout << arr[i] << " + " << arr[j] << " = " << arr[i] + arr[j] << endl;
+			}
+		}
+	}
 
 }
