ebc7bb5b2c0def426e91170ee6da64b83dab76a9
Kevin Lin <kwkevinlin@users.noreply.github.com>
2016-04-11 17:47:23 -0400

    Project setup

---
+++ Next_Largest_Int_In_Binary_Tree.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+struct Node {
+	int data;
+	Node* left;
+	Node* right;
+
+	Node() {
+		data = -1;
+		left = NULL;
+		right = NULL;
+	}
+
+	Node(int num) {
+		data = num;
+		left = NULL;
+		right = NULL;
+	}
+};
+
+int main() {
+
+	/*
+	 * Given a node in a binary tree, find the next
+	 * largest number in that tree.
+	 *
+	 */
+
+
+
+}
