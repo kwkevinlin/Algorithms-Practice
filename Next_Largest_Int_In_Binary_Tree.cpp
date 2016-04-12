2f60ef4b10fae4fe6dfecfe4e29edec25448ec46
Kevin Lin <kwkevinlin@users.noreply.github.com>
2016-04-11 23:13:27 -0400

    Created binary tree and ASCII tree description

--- Next_Largest_Int_In_Binary_Tree.cpp
+++ Next_Largest_Int_In_Binary_Tree.cpp
@@ -20,14 +20,40 @@ struct Node {
 	}
 };
 
+int findNextLargest(int);
+
 int main() {
 
 	/*
 	 * Given a node in a binary tree, find the next
 	 * largest number in that tree.
 	 *
+	 * Example Binary Tree:
+	 *
+	 *           5
+	 *        /     \
+	 *       6       4
+	 *      / \     / \
+	 *     3   9   8   2
+	 *      \
+	 *       1
+	 *
 	 */
 
+	Node* head = new Node(5);
+	head->left = new Node(6);
+	head->left->left = new Node(3);
+	head->left->left->right = new Node(1);
+	head->left->right = new Node(9);
+	head->right = new Node(4);
+	head->right->left = new Node(8);
+	head->right->right = new Node(2);
+
+	cout << "Next largest number: " << findNextLargest(6);
+
+}
+
+int findNextLargest(int findThis) {
 
 
 }
