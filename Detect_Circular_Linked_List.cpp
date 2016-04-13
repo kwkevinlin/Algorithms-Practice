53354700c623e91d697916d48ea2daffe970f4ec
Kevin Lin <kwkevinlin@users.noreply.github.com>
2016-04-12 23:04:51 -0400

    Problem setup

---
+++ Detect_Circular_Linked_List.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+struct Node
+{
+	int data;
+	Node* next;
+
+	Node() {
+		data = -1;
+		next = NULL;
+	}
+	Node(int n) {
+		data = n;
+		next = NULL;
+	}
+};
+
+bool detectCicularLL(Node*);
+
+int main() {
+
+
+}
+
+bool detectCircularLL(Node* head) {
+
+
+
+}
