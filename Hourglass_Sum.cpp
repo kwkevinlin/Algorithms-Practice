d690f0ed69a32d38b95941b19ec54eed43c9f1d7
Kevin Lin <kwkevinlin@users.noreply.github.com>
2016-04-13 00:30:40 -0400

    Problem setup

---
+++ Hourglass_Sum.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	/*
+	 * You are given a 2D array with dimensions 6*6. An hourglass
+	 * in an array is defined as a portion shaped like this:
+	 *
+	 *    a b c
+	 *      d
+	 *    e f g
+	 *
+	 * For example, if we create an hourglass using the number 1
+	 * within an array full of zeros, it may look like this:
+	 *
+	 *    1 1 1 0 0 0
+	 *    0 1 0 0 0 0
+	 *    1 1 1 0 0 0
+	 *    0 0 2 4 4 0
+	 *    0 0 0 2 0 0
+	 *    0 0 1 2 4 0
+     *
+     * Note that there are many hourglasses in the array, for ex:
+     *
+     *    1 1 1     1 1 0     1 0 0     2 4 4
+  	 * 	    1         0         0         2
+	 *	  1 1 1     1 1 0     1 0 0     1 2 4
+	 *
+	 * For which the first three are formed in the first row.
+	 *
+	 * Output:
+	 *     Print the largest sum among all the hourglasses in the
+	 *     array.
+	 *
+	 *     With given input above, output will be:
+	 *         19
+	 *
+	 *
+	 * Adapted from HackerRank:
+	 * 2D Array - DS
+	 * https://www.hackerrank.com/challenges/2d-array
+	 *
+	 */
+
+
+}
+
