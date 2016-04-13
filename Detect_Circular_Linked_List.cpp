c7ed40daed81b5f72eeb67f6d08951b935e20803
Kevin Lin <kwkevinlin@users.noreply.github.com>
2016-04-12 23:10:39 -0400

    Created circular linked list sample

--- Detect_Circular_Linked_List.cpp
+++ Detect_Circular_Linked_List.cpp
@@ -17,15 +17,29 @@ struct Node
 	}
 };
 
-bool detectCicularLL(Node*);
+bool detectCircularLL(Node*);
 
 int main() {
 
+	Node* head = new Node(1);
+	head->next = new Node(2);
 
+	head->next->next = new Node(3);
+	Node* circularHead = head->next->next; //End node links back to Node(3)
+
+	head->next->next->next = new Node(4);
+	head->next->next->next->next = new Node(5);
+	head->next->next->next->next->next = circularHead; //End node links back
+
+	if (detectCircularLL(head)) {
+		cout << "Linked list IS circular!\n";
+	} else {
+		cout << "Linked list is NOT circular!\n";
+	}
 }
 
 bool detectCircularLL(Node* head) {
 
-
+	
 
 }
