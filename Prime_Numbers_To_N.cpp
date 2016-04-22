f242f8590d0e73218ae6ef4cc5c895d1b5bcc42f
Kevin Lin <kwkevinlin@users.noreply.github.com>
2016-04-21 23:15:45 -0400

    Fixed first prime, completed function

--- Prime_Numbers_To_N.cpp
+++ Prime_Numbers_To_N.cpp
@@ -10,7 +10,8 @@ int main() {
 	/*
 	 * Write a function to find all prime numbers until N
 	 *
-	 * Primitive solution of O(N^2) time
+	 * Primitive solution of O(N^2) time, with a few looping
+	 * tricks to achieve not fully O(N^2)
 	 */
 
 	int findPrimeUntil = 50;
@@ -21,10 +22,11 @@ int main() {
 
 void findPrime(int N) {
 
+	cout << 2 << endl; //2 is the first prime number
+
 	bool isPrime = true;
-	int i;
-	for (i = 3; i < N; i = i + 2) {
-		for (int j = 3; j < i; j++) {
+	for (int i = 3; i < N; i += 2) { //Dont need to count evens
+		for (int j = 3; j < i; j += 2) { //Don't need to check evens either
 
 			if (i % j == 0) {
 				//Not a prime
@@ -33,6 +35,7 @@ void findPrime(int N) {
 			}
 		}
 		if (isPrime == true) {
+			//Is a prime
 			cout << i << endl;
 		} else {
 			isPrime = true;
