041352c0e33c9de02648d9d1a4bed1df60bcd5d4
Kevin Lin <kwkevinlin@users.noreply.github.com>
2016-04-22 22:38:10 -0400

    Problem setup

---
+++ Nth_Last_Element_Linked_List.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+/*
+ * Find the Kth element from the end of a linked list.
+ *
+ * For example:
+ * 	   List: 1 -> 2 -> 3 -> 4 -> 5
+ * 	   K = 2
+ * 	   Output: 4
+ *
+ */
+
+
+
+}
