a393036feedc32da25caaaf6d820d0e95eedf7e7
kwkevinlin <kwkevinlin@gmail.com>
2016-04-30 22:26:34 -0400

    Working algorithm

--- Maze_Binary.cpp
+++ Maze_Binary.cpp
@@ -3,6 +3,7 @@
 
 using namespace std;
 
+bool verifyMaze(int[][], int, int);
 int main() {
 
 	/*
@@ -18,13 +19,41 @@ int main() {
 	 *	    If both not, check if at end of path. If yes, return true. Else, false.
 	 */
 
-	int maze[3][3] = {
+	int width = 3;
+	int height = 3;
+	int maze[height][width] = {
 			{0, 1, 0},
 			{0, 1, 1},
 			{0, 0, 1}
 	};
 
+	bool canContinue = true;
 
+	for (int i = 0; i < height; i++) {
+		for (int j = 0; j < width; j++) {
 
-
+			//If find 1
+			if (maze[i][j] == 1) {
+				//Check if below is 1 too
+				if (i + 1 < height && maze[i+1][j] == 1) {
+					//Below is 1
+				} else {
+					//If right is not 1, then below has to be 1, or else cant continue
+					if (j + 1 < width && maze[i][j+1] == 1) {
+						//Right is 1
+					} else if (i == height - 1) {
+						cout << "End of maze\n";
+					} else {
+						canContinue = false;
+					}
+				}
+			}
+		}
+		if (canContinue == false) {
+			cout << "Cannot continue\n";
+			break;
+		}
+	}
 }
+
+
