7340cd2c6b72aa5403a18c70ede61c4642e8d99b
kwkevinlin <kwkevinlin@gmail.com>
2016-04-30 22:27:38 -0400

    Identified and fixed new corner case

--- Maze_Binary.cpp
+++ Maze_Binary.cpp
@@ -19,20 +19,20 @@ int main() {
 	 *
 	 *	Approach:
 	 *	    Whenever there is a 1, check below if there is a 1 or not. If not,
-	 *	    check if right is a 1. If both not, check if at end of path. If yes,
-	 *	    return true. Else, return false.
+	 *	    check if right is a 1. If both not, check if at last row. If yes,
+	 *	    return true. If not, return false.
 	 */
 
 	int maze[height][width] = {
-			{0, 1, 0},
-			{0, 1, 1},
-			{0, 0, 1}
+			{1, 1, 0},
+			{1, 1, 1},
+			{0, 1, 0}
 	};
 
 	if (verifyMaze(maze)) {
 		cout << "Success! Maze can be traversed.\n";
 	} else {
-		cout << "Maze cannot be traversed!\n";
+		cout << "Maze cannot be completed!\n";
 	}
 
 }
@@ -47,15 +47,17 @@ bool verifyMaze(int maze[][width]) {
 				//Check if below is 1 too
 				if (i + 1 < height && maze[i+1][j] == 1) {
 					//Below is 1
+					break; //Don't need to keep checking right
 				} else {
 					//If right is not 1, then below has to be 1, or else maze can't continue
 					if (j + 1 < width && maze[i][j+1] == 1) {
 						//Right is 1
-					} else if (i == height - 1) {
-						//End of maze
+						break; //same as above, don't need to keep checking right
+					} else if (i == height - 1) { //If last row
+						//Then end of maze
 						return true;
 					} else {
-						//Cannot find 1 to the right or bottom of current 1
+						//Cannot find 1 to the right or to the bottom
 						return false;
 					}
 				}
