cc6d2504d92f7727827454c7840fc56bd599813e
Kevin Lin <kwkevinlin@users.noreply.github.com>
2016-05-01 23:18:45 -0400

    Problem setup

---
+++ Holes_in_Numbers.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	/*
+	 * Given a number, find the total number of holes
+	 * in the digits.
+	 *
+	 * For example:
+	 *     0 Hole: 1, 2, 3, 5, 7
+	 *     1 Hole: 0, 4, 6, 9
+	 *     2 Holes: 8
+	 *
+	 *     Sample:
+	 *     24 -> 1
+	 *     1264 -> 2
+	 *     9899 -> 5
+	 *     349 -> 2
+	 *
+	 */
+
+
+}
