077fb60010c4bbbc1449d51f4c3a54582cd732ff
Kevin Lin <kwkevinlin@users.noreply.github.com>
2016-05-06 01:17:38 -0400

    Added complexity, moved to functions

--- Sort_And_Remove_Duplicates_Without_Set.cpp
+++ Sort_And_Remove_Duplicates_Without_Set.cpp
@@ -3,21 +3,45 @@
 
 using namespace std;
 
+void print(int[], int);
+void removeDuplicates(int[], int, int&);
+
 int main() {
 
 	/*
 	 * Sort and remove duplicates in an array without using set
+	 *
+	 * Complexity: n log n for std::sort
 	 */
 
+	//Create test array
 	int num[10] = {8, 1, 4, 9, 0, 4, 9, 9, 3, 5};
 
+	//std::sort
 	sort(num, num + 10);
 
-	for (int i = 0; i < 10; i++) {
+	//Printout
+	print(num, 10);
+
+	//Offset = number of extra duplicate elements
+	int offset = 0;
+
+	//Remove duplicates
+	removeDuplicates(num, 10, offset);
+
+	//Printout
+	print(num, 10 - offset); //Don't display duplicates
+
+}
+
+void print(int num[], int size) {
+	for (int i = 0; i < size; i++) {
 		cout << num[i] << " ";
 	}
+	cout << endl;
+}
 
-	int offset = 0;
+void removeDuplicates(int num[], int size, int& offset) {
 	for (int i = 0; i < 10; i++) {
 		for (int j = i + 1; j < 10; j++) {
 			if (num[j] != num[i]) {
@@ -29,10 +53,4 @@ int main() {
 		}
 		num[i] = num[i + offset];
 	}
-	cout << "\n" << offset << "\n";
-
-	for (int i = 0; i < 10 - offset; i++) { //Don't display duplicates
-		cout << num[i] << " ";
-	}
-
 }
