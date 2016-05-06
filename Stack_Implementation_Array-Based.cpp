4afe2bf72532ff305db4d48dd7571cc8afff86ed
Kevin Lin <kwkevinlin@users.noreply.github.com>
2016-05-06 15:40:23 -0400

    Added test sequence in main, fixed Stack class

--- Stack_Implementation_Array-Based.cpp
+++ Stack_Implementation_Array-Based.cpp
@@ -27,20 +27,30 @@ public:
 			cout << "Stack is full, cannot push more elements.\n";
 			return;
 		}
-		arr[index] = val;
 		index++;
+		size++;
+		arr[index] = val;
 	}
 
 	void pop() {
-		if (index == 0) {
-			cout << "Stack is empty, cannot pop top.\n";
+		if (index < 0) {
+			cout << "Stack is empty, cannot pop more elements.\n";
 			return;
 		}
 		index--;
+		size--;
 	}
 
 	int top() {
+		if (index == -1) {
+			cout << "Stack is empty. Returning -1.\n";
+			return -1;
+		}
 		return arr[index];
+	}
+
+	int getSize() {
+		return size;
 	}
 
 	bool isEmpty() {
@@ -59,5 +69,41 @@ int main() {
 	 * Implementation of an array-based stack.
 	 */
 
+	Stack myStack(5);
 
+	//Push 5 elements
+	myStack.push(1);
+	myStack.push(2);
+	myStack.push(3);
+	myStack.push(4);
+	myStack.push(5);
+
+	//Out of bounds, will return error
+	myStack.push(6);
+
+	//Pop remaining 5 elements
+	myStack.pop();
+	myStack.pop();
+	myStack.pop();
+	myStack.pop();
+	myStack.pop();
+
+	//Out of bounds, will return error
+	myStack.pop();
+
+	//Get number of elements in stack
+	cout << "Size: " << myStack.getSize() << endl;
+
+	//Bool isEmpty
+	cout << "Empty: " << (myStack.isEmpty() ? "true" : "false") << endl;
+
+	//Push new elements
+	myStack.push(15);
+	myStack.push(20);
+
+	//Traverse and pop through stack
+	while(!myStack.isEmpty()) {
+		cout << myStack.top() << endl;
+		myStack.pop();
+	}
 }
