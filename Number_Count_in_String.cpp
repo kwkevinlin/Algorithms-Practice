88103d9b9e3ff058d3af27dc114862576f844ce0
Kevin Lin <kwkevinlin@users.noreply.github.com>
2016-05-05 22:36:05 -0400

    Moved to independent functions; histogram key error

--- Number_Count_in_String.cpp
+++ Number_Count_in_String.cpp
@@ -5,6 +5,9 @@
 
 using namespace std;
 
+string populateStr(string);
+map<int, int> createHistogram(string);
+
 int main() {
 
 	/*
@@ -15,22 +18,11 @@ int main() {
 	string str = "Find the number of occurrences of each number in a string";
 	srand(time(NULL));
 
-	int dice;
-	for (int i = 0; i < str.length(); i++) {
-		dice = rand() % 9;
-		if (dice % 6 == 0) { //Randomly insert numbers based on a probability algorithm
-			str.insert(i, to_string((rand() % 10)));
-		}
-	}
-	cout << "Input: \n" << str << endl;
+	//Insert numbers into string
+	str = populateStr(str);
 
 	//Produce histogram of numbers
-	map<int, int> histogram;
-	for (int i = 0; i < str.length(); i++) {
-		if (str[i] >= '0' && str[i] <= '9') { //ASCII check to see if str[i] is a number
-			histogram[str[i]]++;
-		}
-	}
+	map<int, int> histogram = createHistogram(str);
 
 	//Histogram printout
 	cout << "\nHistogram of each number:\n";
@@ -39,3 +31,33 @@ int main() {
 	}
 }
 
+string populateStr(string str) {
+	int dice;
+	for (int i = 0; i < str.length(); i++) {
+		dice = rand() % 9;
+		if (dice % 6 == 0) { //Randomly insert numbers based on a probability algorithm
+			str.insert(i, to_string((rand() % 10)));
+		}
+	}
+	cout << "Input: \n" << str << endl;
+	return str;
+}
+
+map<int, int> createHistogram(string str) {
+
+	map<int, int> histogram;
+
+	//Pre-populate histogram (0-9)
+	for (int i = 0; i < 10; i++) {
+		histogram[i];
+	}
+
+	//Populate histogram for above numbers
+	for (int i = 0; i < str.length(); i++) {
+		if (str[i] >= '0' && str[i] <= '9') { //ASCII check to see if str[i] is a number
+			histogram[str[i] - '0']++;
+		}
+	}
+
+	return histogram;
+}
