cdfed8067c0be7d639a324f45c070a88e239427b
Kevin Lin <kwkevinlin@users.noreply.github.com>
2016-05-05 22:25:40 -0400

    Changed dice algorithm, added histogram

--- Number_Count_in_String.cpp
+++ Number_Count_in_String.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <stdlib.h>
 #include <time.h>
+#include <map>
 
 using namespace std;
 
@@ -14,14 +15,27 @@ int main() {
 	string str = "Find the number of occurrences of each number in a string";
 	srand(time(NULL));
 
-	int add;
+	int dice;
 	for (int i = 0; i < str.length(); i++) {
-		add = rand() % 100;
-		if (add % 4 == 0) { //Randomly insert numbers based on a probability algorithm
-			str.insert(i, to_string((add % 10)));
+		dice = rand() % 9;
+		if (dice % 6 == 0) { //Randomly insert numbers based on a probability algorithm
+			str.insert(i, to_string((rand() % 10)));
 		}
 	}
-	cout << "String: \n" << str << endl;
+	cout << "Input: \n" << str << endl;
 
+	//Produce histogram of numbers
+	map<int, int> histogram;
+	for (int i = 0; i < str.length(); i++) {
+		if (str[i] >= '0' && str[i] <= '9') { //ASCII check to see if str[i] is a number
+			histogram[str[i]]++;
+		}
+	}
+
+	//Histogram printout
+	cout << "\nHistogram of each number:\n";
+	for (auto kv : histogram) {
+		cout << kv.first - '0' << ": " << kv.second << endl;
+	}
 }
 
