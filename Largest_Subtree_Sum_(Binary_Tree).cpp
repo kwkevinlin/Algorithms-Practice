88c710f9079cd17ba998a92c0ebb1ba7d13a1d24
Kevin Lin <kwkevinlin@users.noreply.github.com>
2016-05-07 17:31:33 -0400

    Problem setup

---
+++ Largest_Subtree_Sum_(Binary_Tree).cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+struct Node {
+	int data;
+	Node* left;
+	Node* right;
+
+	Node(int val) {
+		data = val;
+		left = NULL;
+		right = NULL;
+	}
+};
+
+int main () {
+
+	/*
+	 * Find the largest subtree sum of a binary tree
+	 */
+
+}
