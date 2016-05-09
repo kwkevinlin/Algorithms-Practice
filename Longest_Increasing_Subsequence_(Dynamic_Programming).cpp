4f6321bc1a1dd590baee10a2344e57d200d8c76b
kwkevinlin <kwkevinlin@gmail.com>
2016-05-09 01:53:05 -0400

    Found edge case, debugging

--- Longest_Increasing_Subsequence_(Dynamic_Programming).cpp
+++ Longest_Increasing_Subsequence_(Dynamic_Programming).cpp
@@ -25,16 +25,16 @@ int main() {
 	 */
 
 	//Standard test case
-	//int arr[] = {5, 2, 8, 6, 3, 6, 9, 7};
+	int arr[] = {5, 2, 8, 6, 3, 6, 9, 7};
 
 	//Random test case
-	int arr[8];
-	srand(time(NULL));
-	cout << "Sequence:\n";
-	for (int i = 0; i < 8; i++) {
-		arr[i] = rand() % 10;
-		cout << arr[i] << " ";
-	}
+	//	int arr[8];
+	//	srand(time(NULL));
+	//	cout << "Sequence:\n";
+	//	for (int i = 0; i < 8; i++) {
+	//		arr[i] = rand() % 10;
+	//		cout << arr[i] << " ";
+	//	}
 
 	cout << "\nLongest Increasing Subsequence:\n" << longestIncSubseq(arr, sizeof(arr)/sizeof(arr[0])) << endl;
 
@@ -44,21 +44,37 @@ int longestIncSubseq(int arr[], int size) {
 
 	//maxArr stores max dists so far
 	int maxArr[size];
-	int max = -1;
+	int max = -1, currMax;
 
 	//DP from start to end, O(n^2) solution
 	for (int i = 0; i < size; i++) {
 		//DP base case
 		maxArr[i] = 1;
+		currMax = -1;
 		for (int j = 0; j < i; j++) {
 			if (arr[j] < arr[i]) {
-				maxArr[i] = maxArr[j] + 1;
+				//maxArr[i] = maxArr[j] + 1;
 
-				if (maxArr[i] > max) {
-					max = maxArr[i];
+				//				if (maxArr[i] > max) {
+				//					max = maxArr[i];
+				//				}
+
+				//Get longest increasing subsequence
+				if (maxArr[j] > currMax) {
+					currMax = maxArr[j];
 				}
 			}
+			maxArr[i] = currMax + 1;
+			if (maxArr[i] > max) {
+				max = maxArr[i];
+				//cout << arr[i] << " at " << maxArr[i] << endl;
+			}
 		}
+	}
+
+	//Debug
+	for (int i = 0; i < size; i++) {
+		cout << maxArr[i] << " ";
 	}
 
 	return max;
